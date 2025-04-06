43076fe70be7a7fe9da6628d9065053c31602914
mtnleo <mtnleonardi@gmail.com>
2025-04-06 18:20:53 -0300

    Rough function on changing the imgSrc added, will do the same for all parts of the item

---
+++ func.js
@@ -0,0 +1,6 @@
+function changeImgSrc(imgNumber, newUrl) {
+    document.getElementById("img_" + imgNumber).src = newUrl;
+
+}
+
+changeImgSrc(3, 'https://images.unsplash.com/photo-1741866987680-5e3d7f052b87?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
